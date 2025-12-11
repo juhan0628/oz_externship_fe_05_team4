f9b8bb9978e0ab835746ad30596adaffd7e039d0
박한별 <seohyun062807@naver.com>
2025-12-11 17:05:55 +0900

    feat: Zustand 테스트 코드 등록후 확인

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,13 @@ import {
   JaeEunPage,
   HanbyeolPage,
 } from './pages'
+import { useCountStore } from './store/countStore'
 
 function App() {
+  const count = useCountStore((state) => state.count)
+  const increase = useCountStore((state) => state.increase)
+  const decrease = useCountStore((state) => state.decrease)
+
   return (
     <BrowserRouter>
       <Header />
@@ -20,6 +25,15 @@ function App() {
         <Route path="/JaeEun" element={<JaeEunPage />} />
         <Route path="/HanByeol" element={<HanbyeolPage />} />
       </Routes>
+      <div className="min-h-screen bg-white p-8">
+        <Header />
+
+        <div className="mt-10">
+          <h2>Count: {count}</h2>
+          <button onClick={increase}>+1</button>
+          <button onClick={decrease}>-1</button>
+        </div>
+      </div>
     </BrowserRouter>
   )
 }
