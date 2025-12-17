e1f7883c6b82fed452c752a916c604fbe61abaed
박한별 <seohyun062807@naver.com>
2025-12-17 17:33:20 +0900

    refactor: textarea 컴포넌트 리팩토링

--- src/components/common/Textarea.tsx
+++
@@ -1,9 +0,0 @@
-import { Textarea as ShadTextarea } from '@/components/ui/textarea'
-import { cn } from '@/lib/utils'
-
-export default function Textarea({
-  className,
-  ...props
-}: React.ComponentProps<typeof ShadTextarea>) {
-  return <ShadTextarea className={cn(className)} {...props} />
-}
