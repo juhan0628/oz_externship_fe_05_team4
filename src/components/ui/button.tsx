cec755ff6dcb658a954d6e23e49b4738d3cf0589
박한별 <seohyun062807@naver.com>
2025-12-17 16:17:05 +0900

    refactor: button 컴포넌트 리팩토링

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,41 +17,44 @@ const buttonVariants = cva(
           'bg-gray-400 text-white hover:bg-gray-500 active:bg-gray-600',
         ghost: 'text-primary hover:bg-primary-100',
         success: 'bg-success text-white hover:bg-green-600',
-        disabled: 'bg-gray-disabled text-gray-500 cursor-not-allowed',
       },
       size: {
-        default: 'h-9 px-4 py-2',
         sm: 'h-8 px-3',
+        md: 'h-9 px-4',
         lg: 'h-10 px-6',
         icon: 'h-9 w-9',
+      },
+      fullWidth: {
+        true: 'w-full',
       },
     },
     defaultVariants: {
       variant: 'primary',
-      size: 'default',
+      size: 'md',
     },
   }
 )
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
 
-function Button({
+export function Button({
   className,
   variant,
   size,
+  fullWidth,
   asChild = false,
   ...props
 }: ButtonProps) {
   const Comp = asChild ? Slot : 'button'
+
   return (
     <Comp
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, size, fullWidth }), className)}
       {...props}
     />
   )
 }
-
-export { Button }
