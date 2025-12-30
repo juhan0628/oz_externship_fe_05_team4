039bc535ca3a79f9f12551aa9529c81dd33ca2fa
Nagi-ei <nakt4453@gmail.com>
2025-12-31 04:51:33 +0900

    fix: API 문제로 작동하지 않는 기능 주석 처리 (#54)

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,13 +1,13 @@
-TODO: refreshAccessToken API 수정 후에 주석 해제
-import { bootstrapAuth } from '@/lib/index'
-import { useEffect } from 'react'
+// TODO: refreshAccessToken API 수정 후에 주석 해제
+// import { bootstrapAuth } from '@/lib/index'
+// import { useEffect } from 'react'
 
-const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  useEffect(() => {
-    bootstrapAuth()
-  }, [])
+// const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+//   useEffect(() => {
+//     bootstrapAuth()
+//   }, [])
 
-  return children
-}
+//   return children
+// }
 
-export default AuthProvider
+// export default AuthProvider
