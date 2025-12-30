d942bd28583500f0266e28325ede53668c9f09f4
Nagi-ei <nakt4453@gmail.com>
2025-12-31 04:29:35 +0900

    feat: 내 정보 조회 함수 리팩토링 (#54)

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,4 +1,4 @@
-import { LOG_IN_URL, REFRESH_ACCESS_TOKEN_URL, USER_URL } from '@/data/index'
+import { LOG_IN_URL, USER_URL } from '@/data/index'
 import { token } from '@/lib/index'
 import type { User } from '@/types/user'
 import axios from 'axios'
@@ -18,16 +18,10 @@ const logIn = async (payload: LoginPayload): Promise<User> => {
   token.set(accessToken)
 
   // 유저 정보 조회
-  // TODO: axios 인스턴스 추가 후 리팩토링
-  const userResponse = await axios.get<User>(USER_URL, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  })
-  return userResponse.data
+  return fetchMe()
 }
 
-// TODO: Axios 인스턴스 구현 & API 오류 수정 후에 주석 해제 (리프레시 토큰은 헤더로 받기), export도 추가
+// TODO: Axios 인스턴스 구현 & API 오류 수정 후에 주석 해제 (리프레시 토큰은 헤더로 받기), import & export도 추가
 // const refreshAccessToken = async (): Promise<string> => {
 //   const tokenResponse = await api.post<{ access_token: string }>(
 //     REFRESH_ACCESS_TOKEN_URL
@@ -36,4 +30,14 @@ const logIn = async (payload: LoginPayload): Promise<User> => {
 //   return accessToken
 // }
 
-export { logIn }
+// TODO: Axios 인스턴스 추가 후 리팩토링
+const fetchMe = async (): Promise<User> => {
+  const userResponse = await axios.get<User>(USER_URL, {
+    headers: {
+      Authorization: `Bearer ${token.get()}`,
+    },
+  })
+  return userResponse.data
+}
+
+export { logIn, fetchMe }
