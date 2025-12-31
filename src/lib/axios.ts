e90f327f678f405e65456ca5196cd18ddcd7b27a
박한별 <seohyun062807@naver.com>
2025-12-31 18:15:15 +0900

    feat: 카테고리 필터 API 연결(#55)
    - 최신순 오래된순 변경시 바뀌게 정리

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: '/api', //UPL 나오면 업데이트 예정
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 })
-export default api
