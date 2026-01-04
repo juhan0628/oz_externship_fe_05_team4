ed92388f88fc28efe6c7d0b661c671e3efa3ba9b
juhan0628 <quf062807@gmail.com>
2026-01-04 23:34:00 +0000

    fix: 빌드 에러 수정 (#88)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -164,7 +164,7 @@ export default function MainPage() {
 
       {testChat && (
         <ChatbotContainer
-          initialEntry={testChat}
+          // initialEntry={testChat}
           onClose={() => setTestChat(null)}
         />
       )}
